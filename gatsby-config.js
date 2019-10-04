ef8d1f84ea1c4b19e4e4f81f409203cadf7978ba
gtsdev <kharrison@gtsservices.ca>
2019-10-04 11:27:35 -0400

    connect to contenful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   siteMetadata: {
     title: `Craft Beer`,
@@ -13,6 +16,13 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_Token,
       },
     },
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,3 @@
-// import React from "react"
-// import { Link, graphql } from "gatsby"
-
-// import Layout from "../components/layout"
-
-// import SEO from "../components/seo"
-// import BackgroundSection from "../components/Globals/BackgroundSection"
-// import Info from "../components/Home/Info"
-
-// const IndexPage = ({ data }) => (
-//   <Layout>
-//     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-//     <BackgroundSection
-//       img={data.img.childImageSharp.fluid}
-//       title="Kyle's Craft Brewery"
-//       styleClass="default-background"
-//     />
-//     <Info />
-//   </Layout>
-// )
-
-// export const query = graphql`
-//   {
-//     img: file(relativePath: { eq: "default-background1.jpeg" }) {
-//       childImageSharp {
-//         fluid {
-//           ...GatsbyImageSharpFluid_tracedSVG
-//         }
-//       }
-//     }
-//   }
-// `
-
-// export default IndexPage
-
 import React from "react"
 import { Link, graphql } from "gatsby"
 
