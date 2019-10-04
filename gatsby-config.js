e401df500f1e62cc768ef5a3679a4bbf3a07ae32
gtsdev <kharrison@gtsservices.ca>
2019-10-04 11:34:23 -0400

    capitalized token in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_Token,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
 
