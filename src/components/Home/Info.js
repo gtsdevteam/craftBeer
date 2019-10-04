a563390eb07e2628f582892b101b0b0448583701
gtsdev <kharrison@gtsservices.ca>
2019-10-04 15:26:09 -0400

    products page exists now as well as menu functionality

--- src/components/Home/Info.js
+++ src/components/Home/Info.js
@@ -9,7 +9,15 @@ export default function Info() {
         <Title title="our story"></Title>
         <div className="row">
           <div className="col-10 col-sm-8 mx-auto text-center">
-            <p className="lead text-muted mb-5">woooot</p>
+            <p className="lead text-muted mb-5">
+              This webpage was made using gatsby. I thought I would share some
+              of my favourite beers. Unfortunately I'm not creative enough to
+              come up with a compelling backstory for my fake business so you
+              are stuck reading this drival. I'll probably change this to
+              something that would imply this is a legitimate business with a
+              fully functional shopping cart.. don't buy anything the products
+              aren't real and you wont recieve a refund.{" "}
+            </p>
             <Link to="/about/">
               <button className="btn text-uppercase btn-yellow">
                 about page
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,9 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import Info from "../components/Home/Info"
+import Menu from "../components/Home/Menu"
+import Products from "../components/Home/Products"
+import Contact from "../components/Home/Contact"
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -14,6 +17,9 @@ const IndexPage = ({ data }) => (
       title="Kyle's Craft Brewery"
     />
     <Info />
+    <Menu items={data.menu} />
+    <Products />
+    <Contact />
   </Layout>
 )
 
@@ -23,6 +29,24 @@ export const query = graphql`
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    menu: allContentfulBeerItem {
+      edges {
+        node {
+          id
+          title
+          description {
+            description
+          }
+          price
+          category
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
         }
       }
     }
